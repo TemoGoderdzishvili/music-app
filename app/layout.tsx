f4e8537ba7629b63bdd92185f491068062a08df8
Rasul Kazumovi <125079525+rkazumovi@users.noreply.github.com>
2024-07-21 04:33:25 -0700

    Import fonts in Layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap" rel="stylesheet"></link>
       <body className={inter.className}>{children}</body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,14 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <></>
+    <>
+      <html>
+        <head>
+          <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap" rel="stylesheet" />
+        </head>
+        <body>
+        </body>
+      </html>
+    </>
   );
 }

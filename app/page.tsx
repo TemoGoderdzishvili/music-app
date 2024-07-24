90adf01df912100bd25fd443c91eb4f4d1c6b8b6
Rasul Kazumovi <125079525+rkazumovi@users.noreply.github.com>
2024-07-24 07:12:47 -0700

    create fill and text button component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
+'use client';
 import Image from "next/image";
-import styles from "./page.module.css";
+// import styles from "./page.module.css";
+import React from "react";
+import Button from "./components/Button/Button";
+import styles from "./components/Button/Button.module.css";
 
 export default function Home() {
   return (
     <>
+      <div className="mainDiv">
+        <Button initialText="View playlist" filledText={styles.filledText} textOnlyText="View playlist" />
+      </div>
     </>
   );
 }

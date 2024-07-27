dc7289a7e3f09d30f0a668807f11dd8d1509d214
Rasul Kazumovi <125079525+rkazumovi@users.noreply.github.com>
2024-07-27 09:07:45 -0700

    create ChartCard component

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,42 @@
 'use client';
 import Image from "next/image";
-// import styles from "./page.module.css";
+import styles from "./page.module.css";
 import React from "react";
 import Button from "./components/Button/Button";
-import styles from "./components/Button/Button.module.css";
+import ChartCard from './components/ChartCard/ChartCard';
 
 export default function Home() {
+  const handleCardClick = (title: string) => {
+  };
   return (
     <>
-      <div className="mainDiv">
+      <div className="viewPlaylistDiv">
         <Button initialText="View playlist" filledText={styles.filledText} textOnlyText="View playlist" />
+      </div>
+      <div className="chartCardDiv">
+        <h2 className="topCharts">Top Charts</h2>
+        <div style={{ display: 'flex', gap: '20px' }}>
+          <ChartCard
+            bannerSrc="/images/Top-Hits-2024.jpg"
+            title="Top hits 2024"
+            onClick={() => handleCardClick('Top hits 2024')}
+          />
+          <ChartCard
+            bannerSrc="/images/Chart-Hits-Stars.png"
+            title="Chart Hits Stars"
+            onClick={() => handleCardClick('Chart Hits Stars')}
+          />
+          <ChartCard
+            bannerSrc="/images/Top-Music-Charts.png"
+            title="Top Music Charts"
+            onClick={() => handleCardClick('Top Music Charts')}
+          />
+          <ChartCard
+            bannerSrc="/images/Super-Chart-Hits.png"
+            title="Super Chart Hits"
+            onClick={() => handleCardClick('Super Chart Hits')}
+          />
+        </div>
       </div>
     </>
   );
